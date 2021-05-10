7b58b75c71c0c4482d066f93ac11a71f921ea42d
PrajayG <17574485+PrajayG@users.noreply.github.com>
2021-05-10 21:25:34 +0100

    chore(data): adds test data to separate file

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const timerObject = [
+  {
+    id: 1,
+    workout: 'shadowboxing',
+    duration: 180,
+    tips: ['HANDS UP', 'JABS', '1-2s ONLY'],
+    subtask: '10 seconds, whats going to happen',
+    colours: '',
+    pause: false,
+    quote: "Its hard to wake up at 5am in silk sheets"
+  },
+  {
+    id: 1,
+    workout: 'rest',
+    duration: 300,
+    tips: ['HANDS UP', 'JABS', '1-2s ONLY'],
+    subtask: '10 seconds, whats going to happen',
+    colours: '',
+    pause: false,
+    quote: "Its hard to wake up at 5am in silk sheets"
+  },
+];
+
+export default timerObject;
