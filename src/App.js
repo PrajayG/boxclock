cb16ebce47a435461ffd03dd5956f32200faed5b
PrajayG <17574485+PrajayG@users.noreply.github.com>
2021-05-10 21:24:44 +0100

    feat(meta): adds sass support

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
-import './App.css';
+import './App.scss';
 import React from 'react'
 import Clock from './components/Clock';
+import timerObject from './data';
 
 // have two values in the class, one that controls the current step of the routine you are in
-// and one that that is the steps actual values 
+// and one that that is the steps actual values
 
-const timerObject = [
-  {
-    stage: 1,
-    time: 10,
-    description: '10 seconds, whats going to happen'
-  },
-  {
-    stage: 2,
-    time: 20,
-    description: '3 minutes go'
-  }
-];
+// const timerObject = [
+//   {
+//     stage: 1,
+//     time: 10,
+//     description: '10 seconds, whats going to happen'
+//   },
+//   {
+//     stage: 2,
+//     time: 20,
+//     description: '3 minutes go'
+//   }
+// ];
 
 
 class App extends React.Component {
 
-  state = { 
+  state = {
     routine: timerObject,
     currentStep: 0,
   };
 
-  // it looks like the correct react way to do this is to use .map to render
-  // a lot of components, and then display them??
-  
+
   render() {
   return (
     <div className="App">
